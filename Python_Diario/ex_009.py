219337c1abc507b343f744ba444f062db961eebd
marianyarj <marianyarj@gmail.com>
2024-08-02 22:04:34 +0200

    exercicio 9

---
+++ Python_Diario/ex_009.py
@@ -0,0 +1,9 @@
+""" 9
+Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter multiplicado por n. 
+    Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx".
+"""
+
+def generar_n_caracteres(num, caracter):
+    return num * caracter
+
+print(generar_n_caracteres(5, "x"))
