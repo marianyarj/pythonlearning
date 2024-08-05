356e4ffe3f37e9503af2b6accf6e3a1b7e7d79ff
marianyarj <marianyarj@gmail.com>
2024-08-05 23:42:29 +0200

    exercicio 2

---
+++ Python_Brasil/EstruturaSequencial/ex_002.py
@@ -0,0 +1,10 @@
+"""2
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+"""
+
+def infomar_num():
+
+    num = int(input("Informe um número: "))
+    print(num)
+
+infomar_num()
