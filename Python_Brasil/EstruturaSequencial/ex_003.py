e8fbf286286ca3720a93a02773f7dde9f11ee1bc
marianyarj <marianyarj@gmail.com>
2024-08-05 23:46:12 +0200

    exercicio 3

---
+++ Python_Brasil/EstruturaSequencial/ex_003.py
@@ -0,0 +1,10 @@
+"""3
+Faça um Programa que peça dois números e imprima a soma."""
+
+def soma_dois_num(n1, n2):
+    return n1 + n2
+
+n1 = int(input("Informe o primeiro número: "))
+n2 = int(input("Informe o segundo número: "))
+
+print(soma_dois_num(n1, n2))
